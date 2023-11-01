0f70663c2badcbef763bd32c298c50a1bb30e056
kizato1018 <kizato1018@gmail.com>
2023-11-01 21:25:03 +0800

    Fix: m372

--- m373.cpp
+++ m373.cpp
@@ -11,10 +11,10 @@ int main() {
         for(int i = 0; i < n; i++) {
             cin >> a[i];
         }
-        memset(a, 0, sizeof(dp));
-        memset(dp, 0, sizeof(dp));
         for(int i = 0; i <= k; i++) {
+            dp[i][0] = 0;
             for(int j = 1; j <= n; j++) {
+                dp[i][j] = 0;
                 if(i > 0) dp[i][j] = dp[i-1][j-1];
                 dp[i][j] = max(dp[i][j-1] + a[j-1], dp[i][j]); 
             }
--- test2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	int a = 10;
-    unsigned long long b = 20;
-    short c = max(a, (int)b);
-}
-
